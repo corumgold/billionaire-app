81d7d3e10cad771d5a17d9384fa556eefad9e8a7
Corum Gold <goldcorum@gmail.com>
2023-11-12 13:27:46 -0800

    Clean up write up and DRY up helper funcs

--- src/components/WriteUp.tsx
+++ src/components/WriteUp.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import {
+  actions,
   selectCelebrityName,
   selectCelebrityNetWorth,
 } from "../store/celebritySlice";
 import { selectNetWorth } from "../store/userSlice";
-import { formatCurrency, formatName, getNetWorthRatio } from "../helperFuncs";
+import { formatName, getEmotionalImpactNumber } from "../helperFuncs";
+import { useDispatch } from "react-redux";
 
 const WriteUp: React.FC = () => {
   const userNetWorth = useSelector(selectNetWorth);
   const selectedCelebrityName = useSelector(selectCelebrityName);
   const selectedCelebrityNetWorth = useSelector(selectCelebrityNetWorth);
 
+  const dispatch = useDispatch();
+
   let celebrityName = null;
 
-  const netWorthRatio = getNetWorthRatio(
-    selectedCelebrityNetWorth,
-    userNetWorth
-  );
+  const handleResetGame = () => {
+    dispatch(actions.setCelebrityName(null));
+    dispatch(actions.setCelebrityNetWorth(null));
+  };
 
   if (selectedCelebrityName) {
     celebrityName = formatName(selectedCelebrityName);
@@ -27,13 +31,26 @@ const WriteUp: React.FC = () => {
     <div>
       <p>
         When {celebrityName} purchases an iPhone for $1400, for you this would
-        feel like buying an iPhone for {formatCurrency(1400 / netWorthRatio)}
+        feel like buying an iPhone for{" "}
+        {getEmotionalImpactNumber(
+          selectedCelebrityNetWorth,
+          userNetWorth,
+          1400,
+          "personal"
+        )}
+        .
       </p>
       <p>
-        In other words, in order for {celebrityName} to feel the same emotions
-        when they buy an iPhone, {celebrityName} would need to pay{" "}
-        {formatCurrency(1400 * netWorthRatio)}
+        In other words, in order to elicit the same emotional response as you
+        when purchasing an iPhone, {celebrityName} would need to pay{" "}
+        {getEmotionalImpactNumber(
+          selectedCelebrityNetWorth,
+          userNetWorth,
+          1400,
+          "celebrity"
+        )}
       </p>
+      <button onClick={handleResetGame}>Start Over</button>
     </div>
   );
 };
--- src/helperFuncs.ts
+++ src/helperFuncs.ts
@@ -12,16 +12,29 @@ export function formatName(name: string): string {
     .join(" ");
 }
 
-export function getNetWorthRatio(
+export function getEmotionalImpactNumber(
   largerNetWorth: number | null,
-  smallerNetWorth: number | null
-): number | null {
-  if (!largerNetWorth || !smallerNetWorth) {
+  smallerNetWorth: number | null,
+  itemPrice: number | null,
+  personalOrCeleb: "personal" | "celebrity"
+): number | string | null {
+  if (!largerNetWorth || !smallerNetWorth || !itemPrice || !personalOrCeleb) {
     return null;
   }
 
   if (smallerNetWorth <= 0) {
     smallerNetWorth = 1;
   }
-  return largerNetWorth / smallerNetWorth;
+
+  const netWorthRatio = largerNetWorth / smallerNetWorth;
+
+  if (personalOrCeleb === "personal") {
+    const emotionalImpactNumber = itemPrice / netWorthRatio;
+    if (emotionalImpactNumber < 0.01) {
+      return "less than a penny";
+    } else return formatCurrency(emotionalImpactNumber);
+  } else {
+    const emotionalImpactNumber = itemPrice * netWorthRatio;
+    return formatCurrency(emotionalImpactNumber);
+  }
 }
