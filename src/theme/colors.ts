6b8983c12d6d7f7e000f5d9634e5f9e0e97c4694
Corum Gold <goldcorum@gmail.com>
2023-12-05 11:56:21 -0500

    Add some basic colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+const appColors = {
+  primaryBackground: "#001F3F",
+  secondaryBackground: "#F5F5F5",
+  primaryText: "#FFFFFF",
+  secondaryText: "#333333",
+  accent: "#FFD700",
+};
+
+export default appColors;
